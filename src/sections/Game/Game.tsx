158744111d4a00e591f08b3866dfca8e41fbd253
develookiko <151521342+develookiko@users.noreply.github.com>
2024-08-26 02:37:58 +0300

    Update Game.tsx

--- src/sections/Game/Game.tsx
+++ src/sections/Game/Game.tsx
@@ -12,16 +12,16 @@ import { ProvablyFairModal } from './ProvablyFairModal'
 // import { TransactionModal } from './TransactionModal'
 
 function CustomError() {
-  return (
-    <>
-      <GambaUi.Portal target="error">
-        <GambaUi.Responsive>
-          <h1>😭 Oh no!</h1>
-          <p>Something went wrong</p>
-        </GambaUi.Responsive>
-      </GambaUi.Portal>
-    </>
-  )
+return (
+  <>
+    <GambaUi.Portal target="error">
+      <GambaUi.Responsive>
+        <h1>😭 Ой, нет!</h1>
+        <p>Что-то пошло не так</p>
+      </GambaUi.Responsive>
+    </GambaUi.Portal>
+  </>
+)
 }
 
 /**
@@ -63,83 +63,83 @@ function CustomRenderer() {
     setInfo(false)
   }
 
-  return (
-    <>
-      {info && (
-        <Modal onClose={() => setInfo(false)}>
-          <h1>
-            <img height="100px" title={game.meta.name} src={game.meta.image} />
-          </h1>
-          <p>{game.meta.description}</p>
-          <GambaUi.Button main onClick={closeInfo}>
-            Play
-          </GambaUi.Button>
-        </Modal>
-      )}
-      {provablyFair && (
-        <ProvablyFairModal onClose={() => setProvablyFair(false)} />
-      )}
-      {/* {txModal && (
-        <TransactionModal onClose={() => setTransactionModal(false)} />
-      )} */}
-      <Container>
-        <Screen>
-          <Splash>
-            <img height="150px" src={game.meta.image} />
-          </Splash>
-          <GambaUi.PortalTarget target="error" />
-          {ready && <GambaUi.PortalTarget target="screen" />}
-          <MetaControls>
-            <IconButton onClick={() => setInfo(true)}>
-              <Icon.Info />
+return (
+  <>
+    {info && (
+      <Modal onClose={() => setInfo(false)}>
+        <h1>
+          <img height="100px" title={game.meta.name} src={game.meta.image} />
+        </h1>
+        <p>{game.meta.description}</p>
+        <GambaUi.Button main onClick={closeInfo}>
+          Играть
+        </GambaUi.Button>
+      </Modal>
+    )}
+    {provablyFair && (
+      <ProvablyFairModal onClose={() => setProvablyFair(false)} />
+    )}
+    {/* {txModal && (
+      <TransactionModal onClose={() => setTransactionModal(false)} />
+    )} */}
+    <Container>
+      <Screen>
+        <Splash>
+          <img height="150px" src={game.meta.image} />
+        </Splash>
+        <GambaUi.PortalTarget target="error" />
+        {ready && <GambaUi.PortalTarget target="screen" />}
+        <MetaControls>
+          <IconButton onClick={() => setInfo(true)}>
+            <Icon.Info />
+          </IconButton>
+          <IconButton onClick={() => setProvablyFair(true)}>
+            <Icon.Fairness />
+          </IconButton>
+          <IconButton onClick={() => soundStore.set(soundStore.volume ? 0 : .5)}>
+            {soundStore.volume ? <Icon.Volume /> : <Icon.VolumeMuted />}
+          </IconButton>
+        </MetaControls>
+      </Screen>
+      <LoadingBar />
+      <Controls>
+        <div style={{ display: 'flex', gap: '10px' }}>
+          {/* <div style={{ display: 'flex' }}>
+            <IconButton onClick={() => setTransactionModal(true)}>
+              {loading === -1 ? (
+                <Icon.Shuffle />
+              ) : (
+                <Spinner />
+              )}
             </IconButton>
-            <IconButton onClick={() => setProvablyFair(true)}>
-              <Icon.Fairness />
-            </IconButton>
-            <IconButton onClick={() => soundStore.set(soundStore.volume ? 0 : .5)}>
-              {soundStore.volume ? <Icon.Volume /> : <Icon.VolumeMuted />}
-            </IconButton>
-          </MetaControls>
-        </Screen>
-        <LoadingBar />
-        <Controls>
-          <div style={{ display: 'flex', gap: '10px' }}>
-            {/* <div style={{ display: 'flex' }}>
-              <IconButton onClick={() => setTransactionModal(true)}>
-                {loading === -1 ? (
-                  <Icon.Shuffle />
-                ) : (
-                  <Spinner />
-                )}
-              </IconButton>
-            </div> */}
-            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-              <GambaUi.PortalTarget target="controls" />
-              <GambaUi.PortalTarget target="play" />
-            </div>
+          </div> */}
+          <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+            <GambaUi.PortalTarget target="controls" />
+            <GambaUi.PortalTarget target="play" />
           </div>
-        </Controls>
-      </Container>
-    </>
-  )
+        </div>
+      </Controls>
+    </Container>
+  </>
+)
 }
 
 export default function Game() {
   const { gameId } = useParams()
   const game = GAMES.find((x) => x.id === gameId)
 
-  return (
-    <>
-      {game ? (
-        <GambaUi.Game
-          game={game}
-          errorFallback={<CustomError />}
-          children={<CustomRenderer />}
-        />
-      ) : (
-        <h1>Game not found! 👎</h1>
-      )}
-      <GameSlider />
-    </>
-  )
+return (
+  <>
+    {game ? (
+      <GambaUi.Game
+        game={game}
+        errorFallback={<CustomError />}
+        children={<CustomRenderer />}
+      />
+    ) : (
+      <h1>Игра не найдена! 👎</h1>
+    )}
+    <GameSlider />
+  </>
+)
 }
